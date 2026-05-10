81a34a1013111b1cf3998e703822f4a6301a7084
stardust-zip <nguyenhieu0802@proton.me>
2026-05-10 17:01:02 +0700

    feat: add player

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -18,12 +19,15 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000  # Convert from miliseconds to seconds
---
+++ player.py
@@ -0,0 +1,23 @@
+import pygame
+
+from circleshape import CircleShape
+from constants import LINE_WIDTH, PLAYER_RADIUS
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, radius=PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(
+            surface=screen, color="white", points=self.triangle(), width=LINE_WIDTH
+        )
